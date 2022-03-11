f3bc22a00beb33e89543fb1aa281a14333b40721
mohammedahmed94 <m.ahmed94dev@gmail.com>
2022-03-11 16:58:12 +0200

    managed to show state to each book

--- src/Compnenets/Book.js
+++ src/Compnenets/Book.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from "react";
 import "bulma/css/bulma.min.css";
 
 function Content(props) {
+  const [bookstate, setBookState] = useState("none");
+
+  useEffect(() => {
+    setBookState(props.book.shelf);
+  }, []);
   return (
     <div className="block">
       <div className="card block">
@@ -16,7 +21,36 @@ function Content(props) {
             </h2>
           ))}
         </div>
-        
+
+        <div className="card-footer">
+          <div className="buttons">
+            <button
+              e="button is-light"
+              className={`button ${
+                props.book.shelf === "currentlyReading"
+                  ? "is-success"
+                  : "is-light"
+              }`}
+            >
+              Currently Reading
+            </button>
+            <button
+              className={`button ${
+                props.book.shelf === "wantToRead" ? "is-success" : "is-light"
+              }`}
+            >
+              Want to Read
+            </button>
+            <button
+              className={`button ${
+                props.book.shelf === "read" ? "is-success" : "is-light"
+              }`}
+            >
+              Read
+            </button>
+            <button className="button is-light">None</button>
+          </div>
+        </div>
       </div>
     </div>
   );
