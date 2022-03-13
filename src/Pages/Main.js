425467a112ca84675cdfb080aebb9991690ce2b1
mohammedahmed94 <m.ahmed94dev@gmail.com>
2022-03-13 12:04:19 +0200

    docs: whitespaces and formatting

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -2,28 +2,29 @@ import React, { useState, useEffect } from "react";
 import "bulma/css/bulma.min.css";
 import Shelf from "../Compnenets/Shelf";
 
-
 function Content() {
+  
   return (
     <div className="block  has-background-light">
-      
       <div className="container">
-      <div className="columns ">
-        <div className="column">
-          <Shelf
-            shelf={"currentlyReading"}
-            shelfTitle={"Currently Reading Shelf"}
-          ></Shelf>
-        </div>
-        <div className="column">
-          <Shelf shelf={"wantToRead"} shelfTitle={"Want to Read Shelf"}></Shelf>
-        </div>
-        <div className="column">
-          <Shelf shelf={"read"} shelfTitle={"Read Already Shelf"}></Shelf>
+        <div className="columns">
+          <div className="column">
+            <Shelf
+              shelf={"currentlyReading"}
+              shelfTitle={"Currently Reading Shelf"}
+            ></Shelf>
+          </div>
+          <div className="column">
+            <Shelf
+              shelf={"wantToRead"}
+              shelfTitle={"Want to Read Shelf"}
+            ></Shelf>
+          </div>
+          <div className="column">
+            <Shelf shelf={"read"} shelfTitle={"Read Already Shelf"}></Shelf>
+          </div>
         </div>
       </div>
-      </div>
-     
     </div>
   );
 }
